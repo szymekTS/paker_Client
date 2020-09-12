c01060f2eebe798e11298c3c80b4fb894bf61930
Tomasz Szymański <33700704+szymekTS@users.noreply.github.com>
2020-09-12 12:38:10 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,5 +13,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
